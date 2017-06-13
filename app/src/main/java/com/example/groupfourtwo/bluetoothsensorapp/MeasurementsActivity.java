77d837d5c385cdf00755d4bf41ba07b4c1f86ce6
Nusserle <tobiasnusser@web.de>
2017-06-13 15:33:23 +0200

    Added Measurements-/ Timespan-/ Sensorsettings

---
+++ app/src/main/java/com/example/groupfourtwo/bluetoothsensorapp/MeasurementsActivity.java
@@ -0,0 +1,16 @@
+package com.example.groupfourtwo.bluetoothsensorapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Tobias-PC on 13.06.2017.
+ */
+
+public class MeasurementsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_measurements);
+    }
+}
---
+++ app/src/main/java/com/example/groupfourtwo/bluetoothsensorapp/SensorSettingsActivity.java
@@ -0,0 +1,17 @@
+package com.example.groupfourtwo.bluetoothsensorapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Tobias-PC on 13.06.2017.
+ */
+
+public class SensorSettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensor_settings);
+
+    }
+}
